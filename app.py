5ae8a655c034d42e9b633db6933863e8bd5a11cf
saggy2424 <saggy2424@gmail.com>
2021-05-02 22:53:31 +0530

    Files added in First Web App

---
+++ app.py
@@ -0,0 +1,32 @@
+# from flask_ngrok import run_with_ngrok
+from flask import Flask, request, Response, jsonify, render_template
+# import numpy as np
+# import pickle
+
+root_path = '' #'/content/drive/MyDrive/Colab Notebooks/Inc Prediction App/'
+
+# running the Flask app
+app = Flask(__name__, template_folder=root_path + 'templates/')
+# model = pickle.load(open('model.pkl', 'rb'))
+
+# We need to start ngrok when the app in run. Below line makes the virtual machine URL as public
+# run_with_ngrok(app)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    # int_features = [int(x) for x in request.form.values()]
+    # final_features = [np.array(int_features)]
+    # prediction = model.predict(final_features)
+
+    output = 10000 # round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
+
+app.run()
